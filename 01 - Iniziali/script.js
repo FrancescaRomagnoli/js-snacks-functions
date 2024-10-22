4fe50a9e349411c7760b353031a10f695cf6a318
FrancescaRomagnoli <francesca.romagnoli495@gmail.com>
2024-10-22 17:51:33 +0200

    first exc

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -1,13 +1,21 @@
-/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+/* Scrivi una funzione che accetti un array di nomi e restituisca un nuovo array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
 
+function firstChar(words) {
+  const firstCharList = [];
+
+  words.forEach((word) => {
+    const firstChar = word[0];
+    firstCharList.push(firstChar);
+  });
+  return firstCharList;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(firstChar(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
