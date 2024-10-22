e86326f5c224b531f40334d12e9912578eb8efb6
FrancescaRomagnoli <francesca.romagnoli495@gmail.com>
2024-10-22 18:19:45 +0200

    third exc

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,13 +1,26 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
+const word = "javascript";
 
+const vowels = ["a", "e", "i", "o", "u", "y"];
 
 // Dichiara la funzione qui.
 
+function letterCounter(text, letterList) {
+  const charsList = text.split("");
+  let letterCount = 0;
+
+  charsList.forEach((char) => {
+    if (letterList.includes(char)) {
+      letterCount++;
+    }
+  });
+
+  return letterCount;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(letterCounter(word, vowels));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
